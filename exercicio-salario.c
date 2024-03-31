81088bb126f9991470837fd0cd177d4ad9d53f1d
Natalia <nataliadiasfernande@gmail.com>
2024-03-31 14:24:55 -0300

    corrigindo exercicio

--- exercicio-salario.c
+++ exercicio-salario.c
@@ -7,14 +7,15 @@ int main(void) {
   float valorDoAjuste = salarioAntigo * ajuste;
   float novoSalario = salarioAntigo + valorDoAjuste;
   float valenovo = 28;
-  novoSalario = novoSalario + valenovo;
-  printf("Salário %.2f ", novoSalario);
-
   int diasUteis = 20;
   int horasDeTrabalho = 6;
-  float valorPorHora = novoSalario / (diasUteis * horasDeTrabalho);
+  float valorPorHora ; 
+
+  novoSalario = novoSalario + (valenovo * diasUteis);
+  valorPorHora = novoSalario / (diasUteis * horasDeTrabalho);
+
+  printf("Salário %.2f ", novoSalario);
   printf("\nSalário por hora %.2f ", valorPorHora);
-  
 
   return 0;
 }
